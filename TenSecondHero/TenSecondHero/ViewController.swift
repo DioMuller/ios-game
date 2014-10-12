6429154effee11d51247c62535de7a269945a234
Diogo Muller <diogo.muller@live.com>
2014-10-12 18:36:14 -0300

    Created Project.

---
+++ TenSecondHero/TenSecondHero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TenSecondHero
+//
+//  Created by Diogo Muller on 10/12/14.
+//  Copyright (c) 2014 Old Fat Panda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
