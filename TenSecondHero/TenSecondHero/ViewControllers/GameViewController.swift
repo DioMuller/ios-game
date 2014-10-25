4059886e5b6a0ae1f420508ffc00b4b00f3d8e92
Diogo Muller <diogo.muller@live.com>
2014-10-25 20:08:29 -0200

    Reorganized ViewControllers.

---
+++ TenSecondHero/TenSecondHero/ViewControllers/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  TenSecondHero
+//
+//  Created by Diogo Muller on 10/25/14.
+//  Copyright (c) 2014 Diogo Muller. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let currentView : SKView = self.view as SKView
+        currentView.showsFPS = true
+        
+        let scene : GameScene = GameScene(size: currentView.bounds.size)
+        currentView.presentScene(scene)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
