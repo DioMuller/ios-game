499ddcdd3f530c03a536ccf37d0e924c92a12c64
pucpr <pucpr@LCETMAC0201.local>
2014-10-25 15:07:14 -0200

    Project Organization and base creation

---
+++ TenSecondHero/TenSecondHero/BaseScene.swift
@@ -0,0 +1,13 @@
+//
+//  BaseScene.swift
+//  TenSecondHero
+//
+//  Created by pucpr on 25/10/14.
+//  Copyright (c) 2014 Diogo Muller. All rights reserved.
+//
+
+import SpriteKit
+
+class BaseScene : SKNode {
+    
+}
---
+++ TenSecondHero/TenSecondHero/MainGame.swift
@@ -0,0 +1,14 @@
+//
+//  MainGame.swift
+//  TenSecondHero
+//
+//  Created by pucpr on 25/10/14.
+//  Copyright (c) 2014 Diogo Muller. All rights reserved.
+//
+
+import SpriteKit
+
+class MainGame : SKScene {
+    // Current Scene displayed
+    var currentScene : BaseScene = BaseScene()
+}
