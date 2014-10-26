3871440a320f893e4bae0a4d6e64cdde2e20888a
Diogo Muller <diogo.muller@live.com>
2014-10-26 18:05:20 -0200

    Music on Title Screen.

---
+++ TenSecondHero/TenSecondHero/ViewControllers/TitleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TenSecondHero
+//
+//  Created by Diogo Muller on 10/12/14.
+//  Copyright (c) 2014 Diogo Muller. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class TitleViewController: UIViewController {
+    var mediaPlayer: AVAudioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var file : NSURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("PressStart", ofType: "aifc")!)!
+        var error : NSError?
+        mediaPlayer = AVAudioPlayer(contentsOfURL: file, error: &error )
+        
+        mediaPlayer.prepareToPlay()
+        mediaPlayer.play()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
